90665230473e63b709036e16c95bf41b115b9b7d
florian <florian@sandel.email>
2023-06-08 21:24:38 +0200

    added basic settings and implemented main

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import pygame
 import board
+import settings
 
-pygame.init()
+LEFT = 1
+RIGHT = 3
 
-screen = pygame.display.set_mode((800, 400))
-
-active = True
-
-while active:
-    for event in pygame.event.get():
+def exit_handler(events):
+    active = True
+    for event in events:
         if event.type == pygame.QUIT:
             active = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 active = False
+    return active
 
-pygame.quit()
 
-b = board.Board(3)
-# print(b)
-b[1][1].toCross()
-# b.flushBoard()
-print(b[1][1])
+def gameloop(board_state):
+    active = True
+
+    while active:
+        events = pygame.event.get()
+        active = exit_handler(events)
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+                print("clicked")
+
+
+def main():
+    pygame.init()
+
+    board_state = board.Board
+    screen = pygame.display.set_mode((settings.HEIGHT, settings.WIDTH))
+
+    gameloop(board_state)
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
